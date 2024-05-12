93eae1d95a45e9065088bab5457ad7de5e02e818
Justin Barbee <valkr69@gmail.com>
2024-05-12 02:41:26 -0600

    Adding intial Mod files.

---
+++ Quester.lua
@@ -0,0 +1,7 @@
+--[[
+	QuestSomething
+]]
+
+Quester_File["Quester.lua"] = "0.0.1"
+Quester_Loadtime["Quester.lua"] = GetTime()
+Quester_RegisterRevision("$URL:  $", "$Rev:  $")
---
+++ Quester_pre.lua
@@ -0,0 +1,4 @@
+Quester_File = {}
+Quester_Loadtime = {}
+Quester_File["Quester_pre.lua"] = "1.4.1"
+Quester_Loadtime["Quester_pre.lua"] = GetTime()
